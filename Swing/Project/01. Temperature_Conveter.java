daa4ca52a45025caedbb9bbddc6b211a075eec75
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-13 22:20:27 +0600

    Create 01. Temperature_Conveter.java

---
+++ Swing/Project/01. Temperature_Conveter.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.temperature;
+import javax.swing.*;
+import java.awt.*;
+
+public class C_To_F {
+    
+    public static void main(String[] args) {
+        
+        JFrame f = new JFrame();
+        f.setSize(400, 500);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+    }
+    
+}
