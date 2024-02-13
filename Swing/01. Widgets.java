bbcc291872ebc749cf1259c6310c95075d549a63
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-13 22:25:48 +0600

    Update 01. Widgets.java

--- Swing/01. Widgets.java
+++ Swing/01. Widgets.java
@@ -1,7 +1,8 @@
 // JFrame
 JFrame frame = new JFrame();
-
 frame.setSize(400, 500);         // Height width
 f.setLocationRelativeTo(null);   // Cnter Position
-
 f.setBounds(x,y,h,w);   // Set location and Height Width
+f.setVisible(true);     // Frame visible
+
+
