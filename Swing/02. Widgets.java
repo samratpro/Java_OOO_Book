8b59df1a3e0738141c3785a2cfb366f70cede8a7
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-13 23:06:15 +0600

    Update 02. Widgets.java

--- Swing/02. Widgets.java
+++ Swing/02. Widgets.java
@@ -4,5 +4,8 @@ frame.setSize(400, 500);         // Height width
 f.setLocationRelativeTo(null);   // Cnter Position
 f.setBounds(x,y,h,w);   // Set location and Height Width
 f.setVisible(true);     // Frame visible
-
-
+f.setTitle("Project Name");
+                            // Set Icon Image
+ImageIcon icon = new ImageIcon("C:\\Users\\pc\\Downloads\\calculator.png");
+Image image = icon.getImage();
+f.setIconImage(image);
