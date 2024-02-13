e12742052fb6c1020eb4c0109e4aa88a334d1e87
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-13 22:23:53 +0600

    Create 01. Widgets.java

---
+++ Swing/01. Widgets.java
@@ -0,0 +1,7 @@
+// JFrame
+JFrame frame = new JFrame();
+
+frame.setSize(400, 500);         // Height width
+f.setLocationRelativeTo(null);   // Cnter Position
+
+f.setBounds(x,y,h,w);   // Set location and Height Width
