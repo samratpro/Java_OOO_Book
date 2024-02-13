a4333b28f829531f829ed16b39e0b7d905df0a7c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-02-13 22:32:16 +0600

    Create 01. Intial_project.java

---
+++ Swing/01. Intial_project.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.ProjectName;
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectName {
+    
+    public static void main(String[] args) {
+        
+        JFrame f = new JFrame();
+      
+        f.setSize(400, 500);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        
+    }
+    
+}
